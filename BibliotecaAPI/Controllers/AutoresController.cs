6e8047d5b542073a125a6f11ce337c0f2d7eaa12
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2026-05-06 15:52:20 -0600

    feat: api Autor

--- BibliotecaAPI/Controllers/AutoresController.cs
+++ BibliotecaAPI/Controllers/AutoresController.cs
@@ -1,20 +1,60 @@
+using BibliotecaAPI.Datos;
 using BibliotecaAPI.Entidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BibliotecaAPI.Controllers
 {
     [ApiController]
     [Route("api/autores")]
-    public class AutoresController : ControllerBase
+    public class AutoresController(AppDbContext context) : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Autor> Get()
+        public async Task<IEnumerable<Autor>> Get()
         {
-            return
-            [
-                new() {Id = 1, Nombre = "Hugo Esparza"},
-                new() {Id = 2, Nombre = "Claudia Gonzales"}
-            ];
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor is null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if (id != autor.Id)
+            {
+                return BadRequest("Los id deben de coincidir");
+            }
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var registrosBorrados = await context.Autores.Where(x => x.Id == id).ExecuteDeleteAsync();
+            if (registrosBorrados == 0)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
     }
 }
